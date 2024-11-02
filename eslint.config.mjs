24a75e6976f8bb4eb11d5ba6a1529fa3e1cc1d39
Rebekah Ross <rebekahross123@gmail.com>
2024-11-02 13:16:00 -0600

    updated source type in linter to module to allow for loadTest file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { languageOptions: { globals: globals.node } },
   { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
